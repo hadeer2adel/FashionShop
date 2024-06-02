57d80f40896ea457305e1deae21248718ea06f63
Ahmed Khaled <154894528+AhmedKhaled2711@users.noreply.github.com>
2024-06-01 21:14:51 -0700

    Add main fragments and navigate between  them

--- FashionShop/app/src/main/java/com/example/fashionshop/MainActivity.kt
+++ FashionShop/app/src/main/java/com/example/fashionshop/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.fashionshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView : BottomNavigationView
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
