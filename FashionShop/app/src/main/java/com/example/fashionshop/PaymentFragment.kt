4b52dcda3af3b341d7d10dbf02bbc92626bd92ae
HabibaHagras <101065484+HabibaHagras@users.noreply.github.com>
2024-06-07 10:21:09 +0200

    changes

--- FashionShop/app/src/main/java/com/example/fashionshop/PaymentFragment.kt
+++ FashionShop/app/src/main/java/com/example/fashionshop/PaymentFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.fashionshop.databinding.FragmentPaymentBinding
 
 class PaymentFragment : Fragment() {
@@ -32,6 +33,8 @@ class PaymentFragment : Fragment() {
         }
 
         binding.paymentButton.setOnClickListener {
+            findNavController().navigate(R.id.action_Payment_to_orderDetailsFragment)
+
             when {
                 binding.visaCardCheckBox.isChecked -> {
                     Toast.makeText(activity, "Visa Card selected", Toast.LENGTH_SHORT).show()
